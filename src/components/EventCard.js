f9f65d1fc1fb76f5908fe159fe5826bd789b61fb
gayathridhiya <gayathridhiya1212@gmail.com>
2023-02-11 16:19:35 +0530

    Buttons added for + and -

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import "../styleSheets/EventCardStyle.css";
 import img1 from '../assets/img/ballBackground.gif';
 import img2 from '../assets/img/ballBackground2.gif';
 import img3 from '../assets/img/baseBallBg.gif';
@@ -22,15 +23,18 @@ class EventCard extends Component {
                 <div className="col-md-6 wow slideInUp" data-wow-delay="0.1s">
                 <Card>
                     <Card.Img variant="top" src={this.state.imgArray[randomIndex]} />
-                    <a className="position-absolute top-0 start-0 bg-primary text-white mt-0 py-2 px-4" href="">{item.event_category}</a>
-                    <Card.Body>
+                    <a className="linkStyle position-absolute top-0 start-0 bg-info text-white mt-0 py-2 px-4" href="">{item.event_category}</a>
+                    <Card.Body className="cardBodyStyle">
                         <Card.Title>{item.event_name}</Card.Title>
                         <Card.Text>
-                        <div className="d-flex mb-3">
+                        
                             <small><i className="far fa-calendar-alt text-primary me-2"></i>{item.start_time}</small>
-                        </div>
+                        
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <div className="inOutStyle">
+                        <Button variant="success" className="inStyle">Count me In</Button>
+                        <Button variant="danger" className="outStyle">Count me Out</Button>
+                        </div>
                     </Card.Body>
                 </Card>
                 </div>
