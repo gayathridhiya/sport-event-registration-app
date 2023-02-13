4a0069b735ece5d393266ca0bc062ee9e2390342
gayathridhiya <gayathridhiya1212@gmail.com>
2023-02-13 12:24:34 +0530

    Thank you style added

--- src/components/ThankYou/ThankYou.js
+++ src/components/ThankYou/ThankYou.js
@@ -4,15 +4,11 @@ import './ThankYou.css';
 class ThankYou extends Component {
   render() {
     return (
-      <div className="container-fluid py-5 d-flex h-100">
-        <div className="container py-5">
-          <div className="container py-5">
-            <div className="container py-3">
-
+      <div  className="thankyouStyle">
+        <div className="center-div">
+          <div className='contents'>
               <h1 className="text-center">Thank You!</h1>
-              <p className="lead text-center">Get Ready to Sweat, Laugh and Cheer at Your Next Sports Day Adventure!</p>
-
-            </div>
+              <p className="text-center">Get Ready to Sweat, Laugh and Cheer at Your Sports Day Adventure!</p>
           </div>
         </div>
       </div>
