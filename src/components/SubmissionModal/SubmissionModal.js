e1f54b7e38d93ce7a2eb2a28c320dd6c4f7f3462
gayathridhiya <gayathridhiya1212@gmail.com>
2023-02-13 10:15:03 +0530

    Submission modal enhanced

--- src/components/SubmissionModal/SubmissionModal.js
+++ src/components/SubmissionModal/SubmissionModal.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import { Navigate } from "react-router-dom";
+import Card from 'react-bootstrap/Card';
+import successBookIcon from "../../assets/img/successBook.png";
 
 
 class SubmissionModal extends Component {
@@ -33,11 +35,25 @@ class SubmissionModal extends Component {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <h5>Please confirm your submission for the below events. <br/>Please click on submit to register.</h5>
+                    <h6>Please confirm your submission for the below events. Please click on <b style={{color:"green"}}>Submit</b> to register.</h6>
                     {
-                        Object.values(this.props._selectedevents).map( (itm,idx) => 
+                        Object.values(this.props._selectedevents).map( (item,idx) => 
                         (
-                            <li key={idx}>{itm.item.event_name}</li>
+                            // <li key={idx}>{itm.item.event_name}</li>
+                            <div>
+                            <Card key={item.idx} className="mb-2 d-flex">
+                                <Card.Body className="flex-1">
+                                    <Card.Title className="mt-0 bg-info text-white py-1 px-4">{item.item.event_category}</Card.Title>
+                                    <Card.Title>{item.item.event_name}</Card.Title>
+                                    <Card.Text>
+                                        <small><i className="far fa-calendar-alt text-primary me-2"></i>Event Starts at <b>{new Date(item.item.start_time).toDateString()} - {new Date(item.item.start_time).toLocaleString('en-US', { hour: 'numeric', hour12: true })}</b></small>
+                                        <br/>
+                                        <small><i className="far fa-calendar-alt text-primary me-2"></i>Event Ends by <b>{new Date(item.item.end_time).toDateString()} - {new Date(item.item.end_time).toLocaleString('en-US', { hour: 'numeric', hour12: true })}</b></small>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+
+                        </div>
                         ))
                     }
                     {this.state.showSubmisssionMessage && 
@@ -48,7 +64,7 @@ class SubmissionModal extends Component {
                 <Modal.Footer>
                     <Button disabled = {this.state.showSubmisssionMessage} onClick={() => this.setState({ showSubmisssionMessage : true, redirectToThankYouPage: true})}>Submit</Button>
                     
-                    <Button onClick={() => this.props.onHide(false)}>Go Home</Button>
+                    <Button onClick={() => this.props.onHide(false)}>Edit</Button>
                 </Modal.Footer>
             </Modal>
         )
